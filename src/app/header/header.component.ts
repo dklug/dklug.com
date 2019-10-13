96eb6c70b19d71ae30e9180df4372210fbf983d2
dklug <dklug@ucmerced.edu>
2019-10-13 14:52:20 -0700

    Added cool sun/moon animation on click

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('switchTheme', [
+      state('visible', style({
+        opacity: 1,
+      })),
+      state('invisible', style({
+        opacity: 0,
+        transform: 'rotate(-180deg) scale(0.3)',
+        // transform: 'scale(0.3)',
+      })),
+      transition('* <=> *', animate('.3s ease-in')),
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
+  lightTheme = true;
 
   constructor() { }
 
@@ -14,6 +29,8 @@ export class HeaderComponent implements OnInit {
 
   switchTheme() {
     console.log('switchTheme');
+    this.lightTheme = !this.lightTheme;
+    console.log(`lightTheme ${this.lightTheme}`);
   }
 
 }
